466893e06feacf49ea1d2a16570f5a60fe866a5a
Nik912301 <131801829+Nik912301@users.noreply.github.com>
2024-02-08 22:41:07 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const panelMainMenu = document.getElementById('main_menu');
 
 buttonMainMenu.onclick = function() {
     let displayPosition;
-    if(document.getElementById('main_menu').style.display == 'none')
-        displayPosition = 'block'
-    else
-        displayPosition = 'none'
+    document.getElementById('main_menu').style.display == 'none' ? displayPosition = 'block' : displayPosition = 'none'
     document.getElementById('main_menu').style.display = displayPosition
 }
